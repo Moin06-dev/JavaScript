f42019d56ae277f58e688b20191d8a8ce1c83c46
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-20 00:11:20 +0530

    04_comparission

---
+++ 01_Basics/03_operation.js
@@ -0,0 +1,17 @@
+let value = 3
+let bethe3tonegative = -value
+// console.log(bethe3tonegative);
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2-2);
+// console.log(2**2);
+
+
+let str1 = "hello"
+let str2 = " moinuddin khan"
+
+console.log(str1 + str2);
