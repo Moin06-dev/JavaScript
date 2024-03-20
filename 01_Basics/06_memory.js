3e5f014456fe6ff93797c9c3eb3ab3b247e53ec8
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-21 00:34:36 +0530

    MemoryTypes (Stack,Heap)

---
+++ 01_Basics/06_memory.js
@@ -0,0 +1,28 @@
+
+
+// stack( primitive)
+let myname = "moinuddin khan"
+
+let anothername = myname
+
+
+
+anothername = "mk"
+
+console.log(myname);
+console.log(anothername);
+
+
+// Heap(non - primitive)
+
+let userOne ={
+    email: "moinnvj@gmail.com",
+    upi: "ubv fhjf",
+}
+
+let userTwo = userOne
+
+userTwo.email= "mk1234567890@yahoo.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
