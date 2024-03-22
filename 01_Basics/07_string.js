9fa7f3734ebe571d171d3864f3ea840d65ea66ca
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-23 00:19:25 +0530

    string prototype

---
+++ 01_Basics/07_string.js
@@ -0,0 +1,9 @@
+const { count } = require("console");
+
+const name = "moinkhan"
+const myCount = 50
+console.log(name +  myCount + " the merged");
+
+// new way 
+
+console.log(`hey buddy my name is ${name} and my count is ${count}`);
