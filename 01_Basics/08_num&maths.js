894a14d680b0092defa137dc604886b078acf105
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-23 01:08:26 +0530

    Maths&Number

---
+++ 01_Basics/08_num&maths.js
@@ -0,0 +1,45 @@
+// NUMBER
+
+// const score = 786
+// console.log(`the score is ${score} and type is ${typeof score}`);
+
+
+// // in object way to define 
+// const newscore = new Number(777)
+// console.log(newscore);
+// console.log(typeof newscore);
+
+// console.log(newscore.toString());
+// console.log(newscore.toString().length);
+// console.log(newscore.toFixed(2));
+
+// // its is use to round of the number 
+// const anotherNumber = 255.82726
+// console.log(anotherNumber.toPrecision(4));
+
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-US'));
+// console.log(hundreds.toLocaleString('en-CH'));
+
+
+// Maths
+
+// console.log(Math);
+// console.log(Math.random());
+// console.log(Math.round(3.9));
+// console.log(Math.floor(3.9));
+// console.log(Math.ceil(3.9));
+
+
+console.log(Math.floor(Math.random()*10));
+
+console.log(Math.floor(Math.random()*10 + 1));
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + (min));
