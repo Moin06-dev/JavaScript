7af885082c31b15eef60a9ecf00093fb0e6db901
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-24 00:28:58 +0530

    Date Concept

---
+++ 01_Basics/09_DateinJS.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.getUTCFullYear())
+// console.log(myDate.getTimezoneOffset())
+
+// console.log(typeof myDate);
+
+
+let myBirthDate = new Date(2000, 11, 6);
+console.log(myBirthDate.toDateString());
+
+let sisBirthDate = new Date(2012, 8, 21);
+console.log(sisBirthDate.toDateString());
