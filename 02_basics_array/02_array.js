9a83e90ab504fe3a16f34fdef478ea0bbb4445e0
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-27 00:49:38 +0530

    array method : push,concat,spread,Flat,isarry,from

---
+++ 02_basics_array/02_array.js
@@ -0,0 +1,20 @@
+const marvel_heros = ["thor" , "iron man" , "captain america"]
+const DC_heros = ["superman" , "batman" , "joker"]
+
+// marvel_heros.push(DC_heros)
+// console.log(marvel_heros);
+
+// let newArry = marvel_heros.concat(DC_heros);
+// console.log("Concatenado: ", newArry);
+
+// spred method , glass break idea
+// let newSpreed = [...marvel_heros, ...DC_heros];
+// console.log("Spread: ", newSpreed);
+
+// verification of array 
+console.log(Array.isArray("moinkhan"));
+// convert any datatype into array 
+console.log(Array.from("moinkhan"));
+
+
+
