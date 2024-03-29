cd6976b197c27fbf8f2af66b8e5e9512a52d673d
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-30 00:27:13 +0530

    Disrtuing in object

---
+++ 02_basics_array/05_object3.js
@@ -0,0 +1,13 @@
+const praobject = {
+    name: "moinuddin khan",
+    age: 25,
+    country: "Indian Haq Se"
+
+}
+// console.log(praobject.country);
+// new way and easy way to define lets see
+
+const {country:nk , name : mk} = praobject
+console.log(`${mk} or ${nk}`)
+
+
