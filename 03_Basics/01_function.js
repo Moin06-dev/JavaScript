07e2a24e9f1a2bf3c3eb6f95c9bd27802263e09e
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-30 01:16:21 +0530

    Function ,return aur ()calling ,refrence,parameter,assignment.....

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,32 @@
+
+
+function name(){
+    console.log("M");
+    console.log("O");
+    console.log("I");
+    console.log("N");
+}
+// Refrence
+name
+// function calling   ()
+// name()
+
+
+// function TwoAddNo(num1 , num2){
+// console.log(num1 + num2);
+
+// }
+
+function TwoAddNo(num1 , num2){
+// let result = num1 + num2
+// return result
+
+return num1 + num2
+}
+
+// TwoAddNo()
+// TwoAddNo(5, 7)
+
+const result = TwoAddNo(78 , 90)
+console.log(result);
+
