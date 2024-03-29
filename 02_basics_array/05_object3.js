fc6d4c737d80b1a937722c0d8738362f1c326e40
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-03-30 00:37:35 +0530

    Destructuring Assignment

--- 02_basics_array/05_object3.js
+++ 02_basics_array/05_object3.js
@@ -7,7 +7,21 @@ const praobject = {
 // console.log(praobject.country);
 // new way and easy way to define lets see
 
-const {country:nk , name : mk} = praobject
-console.log(`${mk} or ${nk}`)
+// const {country:nk , name : mk} = praobject
+// console.log(`${mk} or ${nk}`)
 
 
+const sam = {
+    firstName:"Sam",
+    lastName:"Smith",
+    age:30,
+    state: "Maharstra",
+    country: "India",
+
+}
+
+// console.log(sam.state);
+
+// Destructuring Assignment
+const {firstName: fn ,lastName: ln }= sam;
+console.log(`${fn} ${ln}`);
