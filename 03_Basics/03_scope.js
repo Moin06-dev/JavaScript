aed25f8085f9b209f0f024cba5fc1dc9f05f4cbe
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-04-03 00:54:42 +0530

    .

--- 03_Basics/03_scope.js
+++ 03_Basics/03_scope.js
@@ -1,7 +1,7 @@
-if(true){
-    let a = 100;
-    const b = 200;
-    var c = 500;  
+if (true) {
+  let a = 100;
+  const b = 200;
+  var c = 500;
 }
 
 // console.log(a); //a is in let variable so its in block scope
@@ -9,57 +9,47 @@ if(true){
 
 // console.log(c); //but c here var is in var so its very ever its global Scope
 
+function test() {
+  const username = "moinkhan";
 
-
-function test(){
-   const username = "moinkhan";
-
-
-//chilnode of test function
-   function testtwo(){
-    const websitename = " chrome"
+  //chilnode of test function
+  function testtwo() {
+    const websitename = " chrome";
     // here i acces parent name value its because its child its acces parent value
     // console.log(username + websitename)
-}
-//here we print child node value which come under parent function its not work because of scope
-// console.log(websitename)
+  }
+  //here we print child node value which come under parent function its not work because of scope
+  // console.log(websitename)
 
-
-// here we call the child node 
-testtwo()
-// if we not call this the code will not work because of we print in that so we have to call them 
-
+  // here we call the child node
+  testtwo();
+  // if we not call this the code will not work because of we print in that so we have to call them
 }
 // here we call parent function
-test()
+test();
 
+if (true) {
+  const username = "Mk";
+  const Age = 24;
 
-if(true){
-    const username = "Mk"
-    const Age = 24
+  if (username === "MK") {
+    const web = "youtube";
+    // console.log(`${username} and age is ${Age}`);
+  }
 
-
-    if(username === "MK"){
-        const web = "youtube"
-        // console.log(`${username} and age is ${Age}`);
-    }
-
-    // console.log(web);
-
-
-
+  // console.log(web);
 }
 
 // console.log(one(9)) // this will run because here it is ok to declare before the function
-function one(num){
-    return num + 1
+function one(num) {
+  return num + 1;
 }
 
 // console.log(one(9))
 
 // another(5) //ReferenceError: Cannot access 'another' before initialization
-const another = function(num){
-return num + 5
-}
+const another = function (num) {
+  return num + 5;
+};
 
 // console.log(another(5))
