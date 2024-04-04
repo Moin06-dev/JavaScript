736e7724f0c8d5c257deb8fe42960ca4266524a9
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-04-05 00:25:25 +0530

    iife

---
+++ 03_Basics/05_IIFE.js
@@ -0,0 +1,29 @@
+//immediately invoked function expression (iife)
+
+// basic function without iffe so..
+function basicfun(){
+    console.log("This is a basic function");
+}
+basicfun();
+
+
+
+// but in iife we got direct output...
+// ()()in firt bracket we simple write the code and the second one is call the function
+(()=>{
+    console.log('The Immediate Function Expression');
+})();
+
+// ; is compulsary beacuse javascript got confuse hojata hai function mai tho ki konse wala print karo tho error deta hai
+
+// one more example....iife
+
+((username)=>{
+    console.log(`please enter username:${username}`);
+})("Moin Khan");
+
+// more example
+
+const experiment= ( (num1 ,num2)=>{
+console.log(num1 + num2)
+})(10 , 10);
