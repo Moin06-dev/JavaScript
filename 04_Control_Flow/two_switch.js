03fb5b84142c389f7519c1fd639f031b6648e86f
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-04-09 00:26:18 +0530

    switch awesome chiz hai

---
+++ 04_Control_Flow/two_switch.js
@@ -0,0 +1,74 @@
+const birthday = 6
+
+switch (birthday) {
+    case 1:
+         console.log("the date is 1")
+        break;
+    case 2:
+         console.log("the date is 2")
+        break;
+    case 3:
+         console.log("the date is 3")
+        break;
+    case 4:
+         console.log("the date is 4")
+        break;
+    case 5:
+         console.log("the date is five")
+        break;
+    case 6:
+         console.log("the date is 6")
+        break;
+    case 7:
+         console.log("the date is 7")
+        break;
+    case 8:
+         console.log("the date is 8")
+        break;
+    case 9:
+         console.log("the date is 9")
+        break;
+    case 10:
+         console.log("the date is 10")
+        break;
+
+    default:
+        console.log("The number of the day is not between 1 5 and 7 10");
+        break;
+}
+
+
+switch (birthday) {
+    case 1:
+         console.log("the date is 1")
+        break;
+    case 2:
+         console.log("the date is 2")
+        break;
+    case 3:
+         console.log("the date is 3")
+        break;
+    case 4:
+         console.log("the date is 4")
+        break;
+    case 5:
+         console.log("the date is five")
+        break;
+    case 7:
+         console.log("the date is 7")
+        break;
+    case 8:
+         console.log("the date is 8")
+        break;
+    case 9:
+         console.log("the date is 9")
+        break;
+    case 10:
+         console.log("the date is 10")
+        break;
+
+    default:
+        console.log("The number of the day is not between 1 5 and 7 10");
+        //if  the condition not mtch then default going to print...
+        break;
+}
