9cd7a58a955f8c4d415072630ea5fa8dbf2c2371
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-04-17 23:02:19 +0530

    while and do while loop

--- 05_loop/tempCodeRunnerFile.js
+++ 05_loop/tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-for (let index = 0; index < 10; index++) {
-//    if (index == 6 ) {
-//     console.log("im on 6 number buddy")
-//    }
-//     console.log(`the number is:${index}`)
-
-// }
+ index1++
---
+++ 05_loop/while,do_while.js
@@ -0,0 +1,26 @@
+// while loop
+// while (2 <= 4) {
+//     console.log("4 is grater than 2 wow hahahah");
+//     break
+// }
+
+// let number = 2
+// let index = 1
+// while (index <= 10) {
+//     console.log(`${number} * ${index} = ${number * index} `);
+//     index++
+// }
+
+
+// do while loop
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+//     } while (i <= 5) //they run the code first and then check the condition
+const number1= 10
+let index1 = 1;
+do{
+    console.log(`${number1} * ${index1} = ${number1 * index1} `);
+    index1++
+}while(index1 <= 20);
