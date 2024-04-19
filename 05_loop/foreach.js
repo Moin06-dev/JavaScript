49fd3dbfdaa9f55bf307a85f7aa53d2f83e3fa16
KHANMOINUDDIN06 <moinuddinkhan0621@gmail.com>
2024-04-19 23:52:45 +0530

    foreach

---
+++ 05_loop/foreach.js
@@ -0,0 +1,10 @@
+const foreachloop = ["marvel" , "joker" , "DC" , "anime"]
+
+foreachloop.forEach((value)=>{
+console.log(value);
+
+})
+
+foreachloop.forEach((item)=>{
+    console.log(item);
+    })
